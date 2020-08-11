29b2ed8296ebea803b6f81f77eede3dd8a8a555e
Kenny Lam <kinwai.lam730@gmail.com>
2020-08-11 12:06:18 -0400

    Added an example

---
+++ src/Example1/ThreadStart.java
@@ -0,0 +1,27 @@
+package Example1;
+
+public class ThreadStart {
+    public static void main(String[] args) {
+        Runner run1 = new Runner();
+        run1.start();
+
+        Runner run2 = new Runner();
+        run2.start();
+    }
+}
+
+class Runner extends Thread{
+
+    @Override
+    public void run() {
+        for(int i=0; i<10; i++){
+            System.out.println(i);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
