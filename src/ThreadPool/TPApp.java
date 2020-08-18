552ff37b28cdf90ce7ca7f0ea3562c1ba2eb151a
Kenny Lam <kinwai.lam730@gmail.com>
2020-08-17 22:51:38 -0400

    Added await termination try catch and for loop for tasks

--- src/ThreadPool/TPApp.java
+++ src/ThreadPool/TPApp.java
@@ -2,6 +2,7 @@ package ThreadPool;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 class Processor implements Runnable{
 
@@ -29,5 +30,20 @@ public class TPApp {
     public static void main(String[] args) {
         ExecutorService executor = Executors.newFixedThreadPool(2);
 
+        for(int i = 0; i<5; i++){
+            executor.submit(new Processor(i));
+        }
+
+        executor.shutdown();
+
+        System.out.println("All tasks are submitted");
+
+        try {
+            executor.awaitTermination(1, TimeUnit.DAYS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("All tasks completed");
     }
 }
