89f094513bffca7d80fd828fd503608630a35e74
Kenny Lam <kinwai.lam730@gmail.com>
2020-08-17 22:45:21 -0400

    Added Executor service

--- src/ThreadPool/TPApp.java
+++ src/ThreadPool/TPApp.java
@@ -1,4 +1,33 @@
 package ThreadPool;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class Processor implements Runnable{
+
+    private int id;
+    public Processor(int id){
+        this.id = id;
+    }
+    @Override
+    public void run() {
+        System.out.println("Starting: "+ id);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Completed: "+ id);
+    }
+}
+
+
 public class TPApp {
+
+    public static void main(String[] args) {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+    }
 }
